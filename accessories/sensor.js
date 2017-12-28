1a8a6a2ff70bb130876b52a1d3a772b8e83014c7
itsni <34912781+itsni@users.noreply.github.com>
2017-12-28 20:38:25 +0900

    Update sensor.js

--- accessories/sensor.js
+++ accessories/sensor.js
@@ -177,10 +177,9 @@ function HomeAssistantSensorFactory(log, data, client) {
   } else if (typeof data.attributes.unit_of_measurement === 'string' && data.attributes.unit_of_measurement.toLowerCase() === 'ppm' && (data.entity_id.includes('co2') || data.attributes.homebridge_sensor_type === 'co2')) {
     service = Service.CarbonDioxideSensor;
     characteristic = Characteristic.CarbonDioxideLevel;
-  } else if ((typeof data.attributes.unit_of_measurement === 'string' && data.attributes.unit_of_measurement.toLowerCase() === '㎍/㎥') || data.attributes.homebridge_sensor_type === 'airparticulatedensity') {
-    service = Service.AirParticulateDensity;
-    characteristic = Characteristic.AirParticulateSize;
-    
+  } else if ((typeof data.attributes.unit_of_measurement === 'string' && data.attributes.unit_of_measurement.toLowerCase() === '㎍/㎥') || data.attributes.homebridge_sensor_type === 'pm10density') {
+    service = Service.PM10Density;
+    characteristic = Characteristic.PM10Density;
   } else if ((typeof data.attributes.unit_of_measurement === 'string' && data.attributes.unit_of_measurement.toLowerCase() === 'aqi') || data.attributes.homebridge_sensor_type === 'air_quality') {
     service = Service.AirQualitySensor;
     characteristic = Characteristic.AirQuality;
